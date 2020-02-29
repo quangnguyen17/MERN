c05b26de5b9c6adf2af5e467bfcf85125dafe019
Quang Nguyen <quangnguyen@Quangs-MacBook-Pro.local>
2020-02-29 13:00:54 -0800

    added birthday button

--- React/react-demo/src/components/PersonCard.js
+++ React/react-demo/src/components/PersonCard.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
 
 class PersonCard extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            age: this.props.age
+        }
+    }
+
+    birthdayClicked = () => {
+        this.setState({
+            age: this.state.age + 1
+        });
+    }
+
     render() {
         const person = this.props;
         return (
             <div>
                 <h1>{person.lastName}, {person.firstName}</h1>
-                <p>Age: {person.age}</p>
+                <p>Age: {this.state.age}</p>
                 <p>Hair Color: {person.hairColor}</p>
-            </div>
+                <button onClick={this.birthdayClicked}>It's Birthday for {person.firstName}</button>
+            </div >
         );
     }
 }
