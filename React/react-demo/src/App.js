cb7478cbe3c005e071d81b9d4216150a2a4b4936
Quang Nguyen <quangnguyen@Quangs-MacBook-Pro.local>
2020-02-29 13:45:56 -0800

    new sutffs

--- React/react-demo/src/App.js
+++ React/react-demo/src/App.js
@@ -3,7 +3,17 @@ import React from 'react';
 import './App.css';
 // import MyNewComponent from './components/MyNewComponent';
 // import HelloWorld from './components/HelloWorld';
-import PersonCard from './components/PersonCard';
+// import PersonCard from './components/PersonCard';
+
+const PersonCard = (person) => {
+  return (
+    <div>
+      <h1>{person.lastName}, {person.firstName}</h1>
+      <p>Age: {person.age}</p>
+      <p>Hair Color: {person.hairColor}</p>
+    </div>
+  );
+}
 
 const getPersonAt = (index) => {
   const people = [
