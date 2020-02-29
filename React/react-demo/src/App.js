39ba78cc521aee6cf19995350f19e8870a3dc8a7
Quang Nguyen <quangnguyen@Quangs-MacBook-Pro.local>
2020-02-29 12:38:28 -0800

    created PersonCard component

--- React/react-demo/src/App.js
+++ React/react-demo/src/App.js
@@ -1,14 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import MyNewComponent from './components/MyNewComponent';
-import HelloWorld from './components/HelloWorld';
+// import MyNewComponent from './components/MyNewComponent';
+// import HelloWorld from './components/HelloWorld';
+import PersonCard from './components/PersonCard';
 
-function App() {
+const getPersonAt = (index) => {
+  const people = [
+    {
+      firstName: 'Jane',
+      lastName: 'Doe',
+      age: 45,
+      hairColor: 'Black'
+    },
+    {
+      firstName: 'John',
+      lastName: 'Smith',
+      age: 88,
+      hairColor: 'Brown'
+    },
+    {
+      firstName: 'Millard',
+      lastName: 'Fillmore',
+      age: 50,
+      hairColor: 'Brown'
+    },
+    {
+      firstName: 'Maria',
+      lastName: 'Smith',
+      age: 62,
+      hairColor: 'Brown'
+    }
+  ]
+
+  return people[index];
+}
+
+const App = () => {
   return (
     <div className="App">
-      <MyNewComponent />
-      <HelloWorld />
+      <PersonCard firstName={getPersonAt(0).firstName} lastName={getPersonAt(0).lastName} age={getPersonAt(0).age} hairColor={getPersonAt(0).hairColor} />
+      <PersonCard firstName={getPersonAt(1).firstName} lastName={getPersonAt(1).lastName} age={getPersonAt(1).age} hairColor={getPersonAt(1).hairColor} />
+      <PersonCard firstName={getPersonAt(2).firstName} lastName={getPersonAt(2).lastName} age={getPersonAt(2).age} hairColor={getPersonAt(2).hairColor} />
+      <PersonCard firstName={getPersonAt(3).firstName} lastName={getPersonAt(3).lastName} age={getPersonAt(3).age} hairColor={getPersonAt(3).hairColor} />
     </div>
   );
 }
--- React/react-demo/src/components/HelloWorld.js
+++ React/react-demo/src/components/HelloWorld.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 
 class HelloWorld extends Component {
     render() {
-        return (
-            <h1>Hello, World!</h1>
-        );
+        // const element = <h1>Hello, {this.props.first_name}!</h1>;
+        const h1 = <h1>Hello, World!</h1>;
+        return h1;
     }
 }
 
